a543c0bf757e0c3b03ad0cd20d2d47505920b97f
irys33 <irys.dev@gmail.com>
2021-01-01 22:45:43 +0900

    add TODO

--- word_search.py
+++ word_search.py
@@ -69,9 +69,13 @@ def fill_empty(word_table: list[list[str]]) -> list[list[str]]:
 
 
 if __name__ == "__main__":
+    # TODO add input word list
     word_list = ['オオカミ', 'ライオン', 'タマリン', 'カワウソ', 'ヤマネコ', 'イヌ', 'ネコ', 'ネズミ']
+    # TODO add select table size
     word_table = create_word_table(6, 8)
+    # TODO add select word size
     insert_word('オオカミ', word_table, 4)
     insert_word('ライオン', word_table, 4)
     insert_word('タマリン', word_table, 4)
     pprint(fill_empty(word_table))
+
